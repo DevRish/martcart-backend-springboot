46854b0bdb5a4d843a0ff63be3a9d0f43a107463
DevRish <rishavchatterjee1546@gmail.com>
2024-07-07 01:49:52 +0530

    added authorization filter (equivalent to isLoggedIn middleware in the express project)

--- src/main/java/com/devrish/martcart/controller/UserController.java
+++ src/main/java/com/devrish/martcart/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.devrish.martcart.controller;
 
 import com.devrish.martcart.dto.responses.GenericResponse;
+import com.devrish.martcart.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/getUser")
-    public GenericResponse getUserData() {
+    public GenericResponse getUserData(@RequestAttribute(name = "currentUser") User currentUser) {
+        // have access to currently logged in user using currentUser
         return GenericResponse.builder().status(true).message("Route hit successfully").build();
     }
 
---
+++ src/main/java/com/devrish/martcart/filter/AuthFilter.java
@@ -0,0 +1,112 @@
+package com.devrish.martcart.filter;
+
+import com.devrish.martcart.dto.responses.GenericResponse;
+import com.devrish.martcart.model.Session;
+import com.devrish.martcart.model.User;
+import com.devrish.martcart.repository.SessionRepository;
+import com.devrish.martcart.repository.UserRepository;
+import com.devrish.martcart.util.JWTPayload;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Slf4j
+public class AuthFilter extends OncePerRequestFilter {
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+
+            String authHeader = request.getHeader("Authorization");
+
+            if (authHeader == null) {
+                sendUnauthorizedAccessResponse(response);
+                log.error("No Authorization header in request");
+                return;
+            }
+
+            if (authHeader.split("\\s+").length <= 1) {
+                sendUnauthorizedAccessResponse(response);
+                log.error("Invalid auth header");
+                return;
+            }
+
+            String[] jwtTokenParts = authHeader.split("\\.");
+            if (jwtTokenParts.length != 3) {
+                sendUnauthorizedAccessResponse(response);
+                log.error("Invalid jwt token");
+                return;
+            }
+
+            String encodedHeader = jwtTokenParts[0];
+            String encodedPayload = jwtTokenParts[1];
+            String signature = jwtTokenParts[2];
+
+            Mac hashFunc = Mac.getInstance("HmacSHA512");
+            SecretKeySpec secretKey = new SecretKeySpec(jwtSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA512");
+            hashFunc.init(secretKey);
+
+            String data = encodedHeader + "." + encodedPayload;
+            byte[] expectedSignatureBytes = hashFunc.doFinal(data.getBytes(StandardCharsets.UTF_8));
+            String expectedSignature = Base64.getUrlEncoder().withoutPadding().encodeToString(expectedSignatureBytes);
+
+            if (!signature.equals(expectedSignature)) {
+                sendUnauthorizedAccessResponse(response);
+                log.error("Invalid jwt token");
+                return;
+            }
+
+            String decodedPayload = new String(Base64.getDecoder().decode(encodedPayload), StandardCharsets.UTF_8);
+            JWTPayload payload = objectMapper.readValue(decodedPayload, JWTPayload.class);
+
+            Session session = sessionRepository.findById(payload.getId()).orElse(null);
+
+            if (session == null) {
+                sendUnauthorizedAccessResponse(response);
+                log.error("Session invalid or expired");
+                return;
+            }
+
+            User user = session.getUserId();
+
+            request.setAttribute("currentUser", user); // this can be later accessed from the controller
+            filterChain.doFilter(request, response); // continue to controller / next filter
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            sendUnauthorizedAccessResponse(response);
+        }
+
+    }
+
+    private void sendUnauthorizedAccessResponse(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new GenericResponse(false, "Unauthorized Access")));
+    }
+}
