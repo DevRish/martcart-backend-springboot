8f29982710b593bcd613de52083275b202d42ceb
DevRish <rishavchatterjee1546@gmail.com>
2024-08-07 13:28:23 +0530

    tried to setup nodemon-like live reloading which succeeded locally but didn't work with docker, eventually realized that stopping and restarting seems to be a better workflow so commented out the live reloading automation stuff, might use it in future if required, was interesting

--- start_local.sh
+++ start_local.sh
@@ -1,8 +1,9 @@
 export $(cat .env | xargs)
 export DATABASE_URL=mongodb://localhost:27017/martcart
 
-while true; do
-    watch --chgexit "ls -lR ./src | sha1sum" && mvn compile
-done > /dev/null 2>&1 &
+#while true; do
+#    watch --chgexit "ls -lR ./src | sha1sum" && mvn compile
+#done > /dev/null 2>&1 &
 
+mvn compile
 mvn spring-boot:run
